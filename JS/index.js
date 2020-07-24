8090ade455f41447a0fe403a9f158550c47c66e2
Ismael Muñoz <Prosilver05@gmail.com>
2020-07-23 19:50:20 -0500

    changed path to css file, this commit is just to check how github pages works differently to normal browser files

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+const burger = document.getElementById("burger")
+const nav = document.getElementById("navBar")
+let counter = 0
+
+const sleep = (milliseconds) => {
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+burger.onclick = () => {
+
+	if (counter % 2 === 0){
+
+		anime({
+			targets: "#navBar",
+			duration: 1000,
+			translateX: [-200]
+		})
+
+		navBar.style.display = "inline"
+	} else {
+
+		anime({
+			targets: "#navBar",
+			duration: 2000,
+			translateX: [200]
+		})
+
+		sleep(300).then( () =>{
+
+			navBar.style.display = "none"
+
+		})
+
+	}
+
+	counter += 1
+
+}
