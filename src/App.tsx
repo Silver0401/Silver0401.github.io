2b3ba112d29c79de28b59f0016d1eb69689550f0
Ismael Muñoz <Prosilver05@gmail.com>
2021-07-07 02:07:29 -0500

    attempt #6 to remove phone adress bar

--- src/App.tsx
+++ src/App.tsx
@@ -62,6 +62,25 @@ const App: React.FC = () => {
       setTimeout(() => {
         console.log("Positioning page");
         window.scrollTo(0, 1);
+
+        function hideAddressBar() {
+          if (!window.location.hash) {
+            if (document.body.clientHeight < window.outerHeight) {
+              document.body.style.height = window.outerHeight + 50 + "px";
+            }
+
+            setTimeout(function () {
+              window.scrollTo(0, 1);
+            }, 50);
+          }
+        }
+
+        window.addEventListener("load", function () {
+          if (!window.pageYOffset) {
+            hideAddressBar();
+          }
+        });
+        window.addEventListener("orientationchange", hideAddressBar);
       }, 4000);
     });
   }, []);
